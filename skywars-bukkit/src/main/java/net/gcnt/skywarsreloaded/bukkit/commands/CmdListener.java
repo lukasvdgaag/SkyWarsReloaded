6d4afc49123692620a8adbc3e785237885d6411d
lukasvdgaag <lukasvdgaag@gmail.com>
2021-09-26 20:27:26 +0200

    Added command classes.

---
+++ skywars-bukkit/src/main/java/net/gcnt/skywarsreloaded/bukkit/commands/CmdListener.java
@@ -0,0 +1,27 @@
+package net.gcnt.skywarsreloaded.bukkit.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class CmdListener implements CommandExecutor, TabCompleter {
+
+    private final List<Cmd> commands = new ArrayList<>();
+
+    public List<Cmd> getCommands() {
+        return this.commands;
+    }
+
+    public void addCommand(Cmd cmd) {
+        this.commands.add(cmd);
+    }
+
+    public abstract boolean onCommand(CommandSender sender, Command command, String s, String[] args);
+
+    public abstract List<String> onTabComplete(CommandSender sender, Command command, String s, String[] args);
+
+}
