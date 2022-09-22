b2c320fa6a9a53b4d7ee3d699065c28b4b3000f8
TechnicallyCoded <mathiastech.jar@gmail.com>
2022-09-23 00:24:26 +0200

    Add possibility to override the SkyWarsReloaded implementation before onEnable

--- skywars-bukkit/src/main/java/net/gcnt/skywarsreloaded/bukkit/BukkitSkyWarsReloadedPlugin.java
+++ skywars-bukkit/src/main/java/net/gcnt/skywarsreloaded/bukkit/BukkitSkyWarsReloadedPlugin.java
@@ -10,15 +10,36 @@ public class BukkitSkyWarsReloadedPlugin extends JavaPlugin {
      */
 
     private BukkitSkyWarsReloaded plugin;
+    private boolean hasBeenEnabled;
+
+    public BukkitSkyWarsReloadedPlugin() {
+        plugin = new BukkitSkyWarsReloaded(this);
+        hasBeenEnabled = false;
+    }
 
     @Override
     public void onEnable() {
-        plugin = new BukkitSkyWarsReloaded(this);
         plugin.onEnable();
+        hasBeenEnabled = true;
     }
 
     @Override
     public void onDisable() {
         plugin.onDisable();
+    }
+
+    /**
+     * Use this to override initializers in the {@link net.gcnt.skywarsreloaded.SkyWarsReloaded} implementation.
+     * Only use this to make major changes to the plugin such as replacing certain parts of the plugin with your
+     * own logic.
+     * <br><br>
+     * Note: You can't use this method after {@link #onEnable()} has been called on SkyWars. Doing so would
+     * completely break everything.
+     *
+     * @param implementation The new {@link net.gcnt.skywarsreloaded.SkyWarsReloaded} implementation.
+     */
+    public void overrideSkyWarsImplementation(BukkitSkyWarsReloaded implementation) {
+        if (!hasBeenEnabled) this.plugin = implementation;
+        else throw new IllegalStateException("You can't change the SkyWars implementation after onEnable() was already called!");
     }
 }
