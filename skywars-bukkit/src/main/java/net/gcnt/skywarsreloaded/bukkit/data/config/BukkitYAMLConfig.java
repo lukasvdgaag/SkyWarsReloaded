5788d94de80858fca7b54a9f2a01cfaf395448db
GCNT <lukasvdgaag@gmail.com>
2022-10-08 15:53:48 +0200

    Fixed item saving rgb hex colors and unnecessary options

--- skywars-bukkit/src/main/java/net/gcnt/skywarsreloaded/bukkit/data/config/BukkitYAMLConfig.java
+++ skywars-bukkit/src/main/java/net/gcnt/skywarsreloaded/bukkit/data/config/BukkitYAMLConfig.java
@@ -175,15 +175,15 @@ public class BukkitYAMLConfig extends AbstractYAMLConfig {
             BukkitItem item = (BukkitItem) value;
             fileConfiguration.set(property + ".material", item.getMaterial());
             if (item.getAmount() != 1) fileConfiguration.set(property + ".amount", item.getAmount());
-            if (item.getDamage() != 0) fileConfiguration.set(property + ".damage", item.getDamage());
-            if (item.getDurability() != -1) fileConfiguration.set(property + ".durability", item.getDurability());
-            if (item.getDisplayName() != null) fileConfiguration.set(property + ".display-name", item.getDisplayName());
+            if (item.getDamage() > 0) fileConfiguration.set(property + ".damage", item.getDamage());
+            if (item.getDurability() > 0) fileConfiguration.set(property + ".durability", item.getDurability());
+            if (item.getDisplayName() != null && item.getDisplayName().equals("")) fileConfiguration.set(property + ".display-name", item.getDisplayName());
             if (item.getLore() != null) fileConfiguration.set(property + ".lore", item.getLore());
             if (!item.getEnchantments().isEmpty())
                 fileConfiguration.set(property + ".enchantments", item.getEnchantments());
             if (!item.getItemFlags().isEmpty()) fileConfiguration.set(property + ".item-flags", item.getItemFlags());
             if (item.getSkullOwner() != null) fileConfiguration.set(property + ".owner", item.getSkullOwner());
-            if (item.getColor() != null) fileConfiguration.set(property + ".color", item.getColor().getRGB());
+            if (item.getColor() != null) fileConfiguration.set(property + ".color", "#" + Integer.toHexString(item.getColor().getRGB()).substring(2));
         } else {
             fileConfiguration.set(property, value);
         }
